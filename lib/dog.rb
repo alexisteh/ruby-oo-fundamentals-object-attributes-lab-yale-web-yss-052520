6ec0e8ce23334361c84c96c8b6e582c0c7bb4aaa
alexisteh <wenhui.teh23@gmail.com>
2020-05-25 16:37:12 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+    def name=(in_name)
+        @name = in_name
+    end 
+    def name 
+        @name
+    end 
+    def breed 
+        @breed 
+    end 
+    def breed=(in_breed)
+        @breed = in_breed
+    end 
+
+end 
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def name 
+        @name
+    end 
+    def name=(in_name)
+        @name = in_name
+    end 
+    def job 
+        @job 
+    end 
+    def job=(in_job)
+        @job = in_job 
+    end 
+end 
